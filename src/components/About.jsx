50ab52e2c5b8d5998cca799be4097619f883afe5
Adetunji <idowuolatunde32@gmail.com>
2023-03-24 14:00:20 +0100

    fix

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,7 +9,7 @@ import {
     Grid, GridItem, Container
   } from '@chakra-ui/react';
 
-  import { Fade,Zoom} from 'react-awesome-reveal';
+  import { Fade,Slide,Zoom} from 'react-awesome-reveal';
   import join from './images/2.png'
   import career from './images/career.png'
   import job from './images/job.png'
@@ -79,11 +79,10 @@ import {
               our community
               </Text>{' '}
             </Heading>
-            
+            <Slide direction='right'>
             <Grid templateColumns='repeat(2, 1fr)' paddingTop={'35.15px'} gap={4} >
             <GridItem w={{ md:'100%'}} h={{base:'', md:'116.85px'}}  >
 
-            <Zoom left cascade>
                 <Image src={career} width={{base:'64px', md:'104px'}} marginLeft={{base:'17.31px', md:'0px'}}/>
                 <Text
                     paddingTop={'16px'}
@@ -94,11 +93,10 @@ import {
                     lineHeight={'28px'}
                     letterSpacing={'-0.18px'}
                 >Career Guidance</Text>
-            </Zoom>
                 
             </GridItem>
             <GridItem w='100%' h={{base:'', md:'116.85px'}}  >
-            <Zoom>
+           
                 <Image src={supportive} width={{base:'64px', md:'104px'}} marginLeft={{base:'17.31px', md:'0px'}}/>
                 <Text
                 paddingTop={'16px'}
@@ -108,11 +106,11 @@ import {
                 fontWeight={'500'}
                 lineHeight={'28px'}
                 letterSpacing={'-0.18px'}>Supportive Network</Text>
-            </Zoom>
+          
                 
             </GridItem>
             <GridItem w='100%'h={{base:'', md:'116.85px'}}  >
-            <Zoom cascade>
+           
               <Image src={job} width={{base:'64px', md:'104px'}} marginLeft={{base:'17.31px', md:'0px'}}/>
                 <Text 
                     paddingTop={'16px'}
@@ -122,11 +120,11 @@ import {
                     fontWeight={'500'}
                     lineHeight={'28px'}
                     letterSpacing={'-0.18px'}>Job Opportunity</Text>
-            </Zoom>
+            
                 
             </GridItem>
             <GridItem w='100%' h={{base:'', md:'116.85px'}}  >
-            <Zoom cascade>
+          
             <Image src={mentorship} width={{base:'64px', md:'104px'}} marginLeft={{base:'17.31px', md:'0px'}}/>
                 <Text
                     paddingTop={'16px'}
@@ -136,10 +134,12 @@ import {
                     fontWeight={'500'}
                     lineHeight={'28px'}
                     letterSpacing={'-0.18px'}>Mentorship</Text>
-            </Zoom>
+           
                 
             </GridItem>
             </Grid>
+            </Slide>
+            
           </Stack>
         </Flex>
        
